504222b1bbbd4308687b1b79433fb799f5b955ed
Elizabeth Brandt <ebrandt8@uw.edu>
2021-03-15 19:26:25 -0700

    added navbar with home and saved links

---
+++ client/src/components/GlobalNavbar/index.js
@@ -0,0 +1,18 @@
+import { Navbar, Nav } from "react-bootstrap";
+
+function GlobalNavbar() {
+    return (
+        <Navbar bg="primary" variant="dark" expand="lg">
+            <Navbar.Brand href="#home">Google Book Search</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/saved">Saved</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default GlobalNavbar;
