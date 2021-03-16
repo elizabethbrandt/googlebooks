208d427d64738bc84e2d2879dcbe8415854b51fc
Elizabeth Brandt <ebrandt8@uw.edu>
2021-03-15 23:26:19 -0700

    added pages and passed down props

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import Results from "../../components/Results";
 import Search from "../../components/Search";
 
 function Home() {
+
+    const [books, setBooks] = useState([]);
+    console.log("Books", books);
+
     return (
         <div>
-            <Search />
+            <Search setBooks={setBooks} />
+            <Results books={books}/>
         </div>
     )
 }
