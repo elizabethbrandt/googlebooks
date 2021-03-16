c4acb84bde34988fff041a5e279fa3aa269860db
Elizabeth Brandt <ebrandt8@uw.edu>
2021-03-15 19:29:22 -0700

    added container, jumbo, and basic results card

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
-function App() {
-  return (
-    <div className="App">
+import "./main.sass"
+import GlobalNavbar from "./components/GlobalNavbar";
+import Jumbo from "./components/Jumbo";
+import Results from "./components/Results";
+import { Container } from "react-bootstrap";
 
+// import "bootstrap/scss/_variables.scss";
+// import "bootstrap/scss/_mixins.scss";
+// import "bootstrap/scss/_utilities.scss";
+// import "bootstrap/scss/bootstrap-grid.scss";
+// import "bootstrap/scss/bootstrap-reboot.scss"
+// import "bootstrap/scss/_navbar.scss"
+
+function App() {
+
+  return (
+    <div>
+      <GlobalNavbar />
+      <Container className="py-4">
+          <Jumbo />
+          <Results />
+      </Container>
     </div>
   );
+
 }
 
 export default App;
